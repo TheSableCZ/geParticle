a6cb0e9d3e0a40b2ebbe8c2555a35c76754d1bd2
Jan Sobol <hsobol@seznam.cz>
2020-07-29 19:27:15 +0200

    old examples deletion

--- examples/GPU_nbody_sim/utils/Camera.cpp
+++
@@ -1,5 +0,0 @@
-#include "Camera.h"
-
-ge::examples::Camera::Camera()
-{
-}
--- examples/Simple_Particles2/BillboardVertices.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const std::vector<float> particleVertices = {
-										-.5f, -.5f, 0.0f,
-										.5f, -.5f, 0.0f,
-										-.5f, .5f, 0.0f,
-										.5f, .5f, 0.0f, };
